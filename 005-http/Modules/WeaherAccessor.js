c247f75c1858548750d567ae30cf9282860de81d
Andrew Garetov <andrew1gar@yandex.ru>
2023-04-10 22:55:10 +0300

    #005 Задача 2. Реализация

---
+++ 005-http/Modules/WeaherAccessor.js
@@ -0,0 +1,59 @@
+const EventEmitter = require('events');
+const http = require('http');
+
+const API_HOST = 'http://api.weatherstack.com/';
+const API_KEY = process.env.API_KEY;
+const methods = {
+  current: 'current',
+};
+
+const getResponse = (url, callback) => {
+  http.get(url, (response) => {
+    let rawData = ''
+    response.on('data', (chunk) => rawData += chunk)
+    response.on('end', () => {
+      let parsedData = JSON.parse(rawData)
+      if (parsedData.success === false) {
+        const error = parsedData.error.info ? parsedData.error.info : 'Произошла неизвестная ошибка';
+        console.log(`Ошибка при выполнении запроса: ${error}`);
+        return process.exit(-1);
+      }
+      callback(parsedData);
+    });
+  }).on('error', (e) => {
+    console.log(e.message);
+    return process.exit(-1);
+  });
+}
+
+const buildUrl = (method, query) => {
+  return `${API_HOST}${method}?access_key=${API_KEY}&query=${query}`;
+}
+
+const onRequestCurrent = (accessor, city) => {
+  getResponse(buildUrl(methods.current, city), (parsedData) => {
+    if (!parsedData.location || !parsedData.location.name || !parsedData.current || !parsedData.current.temperature) {
+      console.log('Некорректный ответ сервера');
+      return process.exit(-1);
+    }
+    accessor.emit('requestCurrentEnd', {
+      city: parsedData.location.name,
+      temperature: parsedData.current.temperature,
+    })
+  })
+}
+
+class WeatherAccessor extends EventEmitter {
+  requestCurrent(city) {
+    this.emit('requestCurrent', city);
+  }
+}
+const getAccessor = () => {
+  const accessor = new WeatherAccessor();
+  accessor.on('requestCurrent', (city) => onRequestCurrent(accessor, city));
+  return accessor;
+}
+
+module.exports = {
+  create: () => getAccessor(),
+};
---
+++ 005-http/index.js
@@ -0,0 +1,21 @@
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+
+const argv = yargs(hideBin(process.argv))
+  .option('city', {
+    alias: 'c',
+    required: true,
+    description: 'город'
+  })
+  .usage('Usage: node $0 -c="place" [--city="place"] [--help]')
+  .example('node $0 -c="Moscow"')
+  .argv;
+
+const weatherAccessorsFactory = require('./Modules/WeaherAccessor');
+const weatherAccessor = weatherAccessorsFactory.create();
+
+weatherAccessor.on('requestCurrentEnd', result => {
+  console.log(`Текущая температура в городе ${result.city}: ${result.temperature}°C`);
+});
+
+weatherAccessor.requestCurrent(argv.city);
