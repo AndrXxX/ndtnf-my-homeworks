ba44fce67b2a718e9418a27233e0736ed8c8d341
Andrew Garetov <andrew1gar@yandex.ru>
2023-04-19 21:40:14 +0300

    #007 Использование error404Middleware

--- 007-middleware/index.js
+++ 007-middleware/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const apiRouter = require('./routes/api');
+const error404Middleware = require("./middleware/error404");
 
 const app = express();
+
 app.use(express.json());
 app.use('/api', apiRouter);
+app.use(error404Middleware);
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT)
--- 007-middleware/routes/api/books.js
+++ 007-middleware/routes/api/books.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const booksStore = require("../../Store/BookStore");
+const error404Middleware = require("../../middleware/error404");
 
 router.get('/', (req, res) => {
   res.json(booksStore.getAll());
 });
 
-router.get('/:id', (req, res) => {
-  const book = booksStore.get(req.params.id);
-  if (book) {
-    return res.json(book);
-  }
-  res.code = 404;
-  res.json('404 | Not found');
-});
+router.get('/:id',
+  (req, res, next) => {
+    const book = booksStore.get(req.params.id);
+    if (book) {
+      return res.json(book);
+    }
+    next();
+  },
+  error404Middleware,
+);
 
 router.post('', (req, res) => {
   const book = booksStore.add(req.body);
@@ -21,25 +24,29 @@ router.post('', (req, res) => {
   res.json(book);
 });
 
-router.put('/:id', (req, res) => {
-  const book = booksStore.get(req.params.id);
-  if (book) {
-    book.fillByParams(req.body);
-    res.code = 201;
-    return res.json(book);
-  }
-  res.code = 404;
-  res.json('404 | Not found');
-});
+router.put('/:id',
+  (req, res, next) => {
+    const book = booksStore.get(req.params.id);
+    if (book) {
+      book.fillByParams(req.body);
+      res.code = 201;
+      return res.json(book);
+    }
+    next();
+  },
+  error404Middleware,
+);
 
-router.delete('/:id', (req, res) => {
-  const result = booksStore.delete(req.params.id);
-  if (result) {
-    res.code = 201;
-    return res.json('ok');
-  }
-  res.code = 404;
-  res.json('404 | Not found');
-})
+router.delete('/:id',
+  (req, res, next) => {
+    const result = booksStore.delete(req.params.id);
+    if (result) {
+      res.code = 201;
+      return res.json('ok');
+    }
+    next();
+  },
+  error404Middleware,
+);
 
 module.exports = router;
