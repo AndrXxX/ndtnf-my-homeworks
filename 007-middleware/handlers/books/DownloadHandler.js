f327ab0ab703f663b899e69cbbe9c3ebec4188d9
Andrew Garetov <andrew1gar@yandex.ru>
2023-04-20 22:58:55 +0300

    #007 Правка метода скачивания книги

--- 007-middleware/handlers/books/DownloadHandler.js
+++ 007-middleware/handlers/books/DownloadHandler.js
@@ -1,7 +1,5 @@
 const booksStore = require("../../store/BookStore");
 const path = require('path');
-const mime = require('mime');
-const fs = require('fs');
 const uploadDirAccessor = require('../../utils/UploadDirAccessor');
 
 module.exports = (req, res, next) => {
@@ -13,9 +11,5 @@ module.exports = (req, res, next) => {
   if (!uploadDirAccessor.checkAccess(file)) {
     return next();
   }
-
-  res.setHeader('Content-disposition', 'attachment; filename=' + path.basename(file));
-  res.setHeader('Content-type', mime.getType(file));
-  const filestream = fs.createReadStream(file);
-  filestream.pipe(res);
+  res.download(file, path.basename(file));
 };
