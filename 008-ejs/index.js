5e9e34f0aa986e27304b45ec5bb3da9771dc664a
Andrew Garetov <andrew1gar@yandex.ru>
2023-04-22 16:27:34 +0300

    #008 Добавлен роут главной страницы

--- 008-ejs/index.js
+++ 008-ejs/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const apiRouter = require('./routes/api');
+const indexRouter = require('./routes/index');
+// const booksRouter = require('./routes/books');
 const error404Middleware = require("./middleware/error404");
 const { port } = require('./config');
 const uploadDirAccessor = require('./utils/UploadDirAccessor');
 
 const app = express();
+app.set("view engine", "ejs");
 uploadDirAccessor.createBookUploadDir();
 
 app.use(express.json());
 app.use('/api', apiRouter);
+app.use('/', indexRouter);
+// app.use('/books', booksRouter);
 app.use(error404Middleware);
 
 app.listen(port);
