0ee025cd4e3c980b08f434da436e21d49a4c91a0
Andrew Garetov <andrew1gar@yandex.ru>
2023-04-24 23:12:19 +0300

    #008 Удален лишний фрагмент

--- 008-ejs/routes/handlers/CreateBookHandler.js
+++ 008-ejs/routes/handlers/CreateBookHandler.js
@@ -1,7 +1,6 @@
 const booksStore = require("../../store/BookStore");
 
 module.exports = (req, res) => {
-  console.log(req);
   const params = req.body;
   if (req.file) {
     const { path } = req.file;
