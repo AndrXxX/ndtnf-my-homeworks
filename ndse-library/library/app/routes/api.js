0734078406cdb206f7967d9754d051f99acd70a0
Andrew Garetov <andrew1gar@yandex.ru>
2023-05-17 22:14:27 +0300

    #012 Подключение authMiddleware

--- ndse-library/library/app/routes/api.js
+++ ndse-library/library/app/routes/api.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const userRouter = require('./api/user');
 const booksRouter = require('./api/books');
 const error404Middleware = require("../middleware/api404");
+const authMiddleware = require("../middleware/apiAuth");
 
 router.use('/user', userRouter);
+router.use(authMiddleware);
 router.use('/books', booksRouter);
 router.use(error404Middleware);
 
