c2f64cc76b131adc6bd44aaaa6d0ff4aca91849f
Andrew Garetov <andrew1gar@yandex.ru>
2023-05-17 21:28:51 +0300

    #012 Добавлен HashGenerator

---
+++ ndse-library/library/app/utils/HashGenerator.js
@@ -0,0 +1,14 @@
+import bcrypt from 'bcrypt';
+const saltRounds = 10;
+
+module.exports = {
+  isValid(password, hash) {
+    return bcrypt.compareSync(password, hash);
+  },
+  generate(password) {
+    if (!password) {
+      throw new Error("Пароль не может быть пустым");
+    }
+    return bcrypt.hashSync(password, saltRounds);
+  }
+};
