d1d12329eb3fa21a63b5102ecb29efbafa6560bf
Andrew Garetov <andrew1gar@yandex.ru>
2023-05-18 00:18:13 +0300

    #012 Подключение expressSession

--- ndse-library/library/app/index.js
+++ ndse-library/library/app/index.js
@@ -1,16 +1,30 @@
 const express = require('express');
+const expressSession = require('express-session');
+const passport = require('passport');
+
 const apiRouter = require('./routes/api');
 const indexRouter = require('./routes/index');
 const booksRouter = require('./routes/books');
 const userRouter = require('./routes/user');
+
 const mongoose = require('mongoose');
 const error404Middleware = require("./middleware/web404");
-const { port, dbUrl } = require('./config');
+const { cookieSecret, port, dbUrl } = require('./config');
 const uploadDirAccessor = require('./utils/UploadDirAccessor');
 const auth = require('./boot/auth');
 
 const app = express();
 auth();
+
+app.use(expressSession({
+  secret: cookieSecret,
+  resave: false,
+  saveUninitialized: false,
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.set("view engine", "ejs");
 uploadDirAccessor.createBookUploadDir();
 
