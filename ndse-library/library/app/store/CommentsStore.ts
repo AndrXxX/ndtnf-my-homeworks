46d4edf83bab43d501238499756b94723691e978
Andrew Garetov <andrew1gar@yandex.ru>
2023-07-02 01:52:00 +0300

    #003 Переход на TS в CommentsStore

---
+++ ndse-library/library/app/store/CommentsStore.ts
@@ -0,0 +1,18 @@
+import { Comment, CommentModel } from "models/Comment";
+
+export type CommentsFilter = {
+  refTypeId?: string;
+}
+
+export class CommentsStore {
+  async getComments(limit: number, params: CommentsFilter) {
+    return CommentModel.find(params).sort({ 'date': -1, '_id': -1 }).limit(limit).select('-__v');
+  }
+  async create(params: Comment): Promise<Comment> {
+    const comment = new CommentModel(params);
+    await comment.save();
+    return comment;
+  }
+}
+
+export const commentsStore = new CommentsStore();
