6953861df7887c783f9d65d9a19d8fa2188ec45c
Andrew Garetov <andrew1gar@yandex.ru>
2023-07-02 01:35:53 +0300

    #003 Переход на TS в apiAuth

--- ndse-library/library/app/middleware/apiAuth.ts
+++ ndse-library/library/app/middleware/apiAuth.ts
@@ -1,4 +1,6 @@
-module.exports =  (req, res, next) => {
+import { Request, Response } from "express";
+
+export default (req: Request, res: Response, next: () => void) => {
   if (!req.isAuthenticated || !req.isAuthenticated()) {
     return res.redirect('/api/user/login');
   }
