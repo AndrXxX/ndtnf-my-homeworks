eaf03feb6bf4cff9f433e20dc3f535e3fc314ff5
Andrew Garetov <andrew1gar@yandex.ru>
2023-07-16 17:29:03 +0300

    #003 Переход на TS в routes/index

--- ndse-library/library/app/routes/index.ts
+++ ndse-library/library/app/routes/index.ts
@@ -1,4 +1,5 @@
-const express = require('express');
+import express from "express";
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -7,4 +8,4 @@ router.get('/', (req, res) => {
     })
 });
 
-module.exports = router;
+export default router;
