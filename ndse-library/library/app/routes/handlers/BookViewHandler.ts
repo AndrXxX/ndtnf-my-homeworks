544e0a6cc31f76eb37fd531e52c75adc4d01f040
Andrew Garetov <andrew1gar@yandex.ru>
2023-07-16 17:43:33 +0300

    #003 Переход на TS в routes/handlers/BookViewHandler

---
+++ ndse-library/library/app/routes/handlers/BookViewHandler.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import { User } from "models/User";
+import { booksStore } from "store/BooksStore";
+import countersFactory from "utils/CountersAccessor";
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+  const counter = countersFactory.getAccessor();
+  await counter.incr(req.params.id);
+  const book = await booksStore.getBook(req.params.id);
+  if (!book) {
+    return next();
+  }
+  res.render("books/view", {
+    title: "Книги | Просмотр",
+    book,
+    count: await counter.get(req.params.id),
+    username: (req.user as User).username,
+  });
+};
