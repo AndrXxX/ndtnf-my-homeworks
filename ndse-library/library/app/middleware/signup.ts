e31433258397a8f1cbba630f50b230b0a034131e
Andrew Garetov <andrew1gar@yandex.ru>
2023-07-16 15:16:16 +0300

    #003 Переход на TS в signup

---
+++ ndse-library/library/app/middleware/signup.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import { usersStore } from "store/UsersStore";
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+  let user = await usersStore.getUser({ username: req.body.user.username });
+  if (user) {
+    return res.status(201).json({
+      error: `Пользователь с логином ${user.username} уже зарегистрирован`,
+      info: null,
+      success: false,
+    });
+  }
+  user = await usersStore.createUser(req.body.user);
+  req.user = user;
+  res.status(201).json({
+    error: null,
+    info: "Пользователь зарегистрирован",
+    success: true,
+  });
+  return next();
+}
