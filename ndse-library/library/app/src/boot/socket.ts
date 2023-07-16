6bc3ec8d21b8cfbbdc09e19c64e4a87cf00d3817
Andrew Garetov <andrew1gar@yandex.ru>
2023-07-17 02:07:32 +0300

    #003 Использование CommentsService

--- ndse-library/library/app/src/boot/socket.ts
+++ ndse-library/library/app/src/boot/socket.ts
@@ -1,19 +1,22 @@
 import { Server, Socket } from "socket.io";
-import { commentsStore } from "../store/CommentsStore";
+import container from "../infrastructure/container";
+import { CommentsService } from "../modules/comments/CommentsService";
 
 const onLoadBookDiscussion = async (socket: Socket, bookId: string) => {
-  const comments = await commentsStore.getComments(5, { refTypeId: bookId});
+  const commentsService = container.get(CommentsService);
+  const comments = await commentsService.getComments(5, { refTypeId: bookId});
   comments && socket.emit('load-book-discussion', comments);
 }
 
 const onBookDiscussion = async (socket: Socket) => {
+  const commentsService = container.get(CommentsService);
   const {bookId} = socket.handshake.query;
   await onLoadBookDiscussion(socket, bookId as string);
 
   console.log(`Socket bookId: ${bookId}`);
   socket.join(bookId);
   socket.on('book-discussion', async (msg) => {
-    const comment = await commentsStore.create(msg);
+    const comment = await commentsService.create(msg);
     if (comment) {
       socket.to(bookId).emit('book-discussion', comment);
       socket.emit('book-discussion', comment);
