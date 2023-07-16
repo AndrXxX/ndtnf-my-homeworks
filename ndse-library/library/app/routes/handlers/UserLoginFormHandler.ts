41ed50c3ef4a3770766922e4798a4f9c9481af4b
Andrew Garetov <andrew1gar@yandex.ru>
2023-07-16 17:44:04 +0300

    #003 Переход на TS в routes/handlers/UserLoginFormHandler

--- ndse-library/library/app/routes/handlers/UserLoginFormHandler.ts
+++ ndse-library/library/app/routes/handlers/UserLoginFormHandler.ts
@@ -1,4 +1,6 @@
-module.exports = async (req, res) => {
+import { Request, Response } from "express";
+
+export default async (req: Request, res: Response) => {
   res.render('user/login', {
     title: "Авторизация",
     user: {},
