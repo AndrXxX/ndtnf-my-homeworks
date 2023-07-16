887ddaed89b2a439022b7bb7863b1ed70c1d7e41
Andrew Garetov <andrew1gar@yandex.ru>
2023-07-16 15:27:12 +0300

    #003 Переход на TS в boot/socket

--- ndse-library/library/app/boot/socket.ts
+++ ndse-library/library/app/boot/socket.ts
@@ -1,13 +1,14 @@
-const commentsStore = require("../store/CommentsStore");
+import { Server, Socket } from "socket.io";
+import { commentsStore } from "store/CommentsStore";
 
-const onLoadBookDiscussion = async (socket, bookId) => {
+const onLoadBookDiscussion = async (socket: Socket, bookId: string) => {
   const comments = await commentsStore.getComments(5, { refTypeId: bookId});
   comments && socket.emit('load-book-discussion', comments);
 }
 
-const onBookDiscussion = async (socket) => {
+const onBookDiscussion = async (socket: Socket) => {
   const {bookId} = socket.handshake.query;
-  await onLoadBookDiscussion(socket, bookId);
+  await onLoadBookDiscussion(socket, bookId as string);
 
   console.log(`Socket bookId: ${bookId}`);
   socket.join(bookId);
@@ -20,13 +21,13 @@ const onBookDiscussion = async (socket) => {
   });
 }
 
-const onDisconnect = (socket, id) => {
+const onDisconnect = (socket: Socket, id: string) => {
   socket.on('disconnect', () => {
     console.log(`Socket disconnected: ${id}`);
   });
 }
 
-module.exports = (io) => {
+export default (io: Server) => {
   io.on('connection', async (socket) => {
     const {id} = socket;
     console.log(`Socket connected: ${id}`);
