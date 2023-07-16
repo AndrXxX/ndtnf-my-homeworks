9da052e94c38caebac16b29ee4bd26fbcf212cc0
Andrew Garetov <andrew1gar@yandex.ru>
2023-07-16 15:43:56 +0300

    #003 Переход на TS в routes/api/handlers/BookDownloadHandler

---
+++ ndse-library/library/app/routes/api/handlers/BookDownloadHandler.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from "express";
+import path from "path";
+import { booksStore } from "store/BooksStore";
+import uploadDirAccessor from "utils/UploadDirAccessor";
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+  const book = await booksStore.getBook(req.params.id);
+  if (!book || !book.fileName) {
+    return next();
+  }
+  const file = path.join(__dirname, "../../../", book.fileName);
+  if (!uploadDirAccessor.checkAccess(file)) {
+    return next();
+  }
+  res.download(file, path.basename(file));
+};
